62e9313ca259624346861f534e4eb7925b4adb70
cohub77 <cohub77@gmail.com>
2022-09-10 00:16:13 -0600

    added 2 new heroes to the list

--- Superhero/config/urls.py
+++ Superhero/config/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from hero.views import HulkView, IndexView, IronManView, BlackWidow
+from hero.views import HulkView, IndexView, IronManView, BlackWidow, HindsightLad, ThreeDMan
 
 urlpatterns = [
     path('hulk',        HulkView.as_view()),
     path('ironman',        IronManView.as_view()),
     path('blackwidow',        BlackWidow.as_view()),
+    path('hindsightlad',        HindsightLad.as_view()),
+    path('3dman',        ThreeDMan.as_view()),
+
     path('',            IndexView.as_view()),
 ]
--- Superhero/hero/views.py
+++ Superhero/hero/views.py
@@ -28,7 +28,26 @@ class BlackWidow(TemplateView):
             'id': 'idk',
             'image': '/static/images/black_widow.jpg'
         }
+class HindsightLad(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Hindsight Lad',
+            'id': 'Carlton LaFroyge',
+            'image': '/static/images/Origin-of-Hindsight-Lad.png'
+        }
+class ThreeDMan(TemplateView):
+    template_name = 'hero.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            'title': '3-D Man',
+            'id': 'Chuck Chandler',
+            'image': '/static/images/3dman.png'
+        }
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'heroes.html'        
+
