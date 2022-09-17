9fc92510154c62e01d03ce70924c5c5bb1a95745
cohub77 <cohub77@gmail.com>
2022-09-16 19:15:36 -0600

    moved files to correct folder

--- Hero2/config/config/urls.py
+++ Hero2/config/config/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('blackwidow',        BlackWidow.as_view()),
     path('hindsightlad',        HindsightLad.as_view()),
     path('3dman',        ThreeDMan.as_view()),
-
     path('',            IndexView.as_view()),
 ]
