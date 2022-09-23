f0300f6332835e547a5b09af40f7c89b43b07d63
cohub77 <cohub77@gmail.com>
2022-09-23 16:33:14 -0600

    Added heros to databse

--- 05/Notes/config/settings.py
+++ 05/Notes/config/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = 'static_assets/'
-
+MEDIA_ROOT = 'media_assets/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- 05/Notes/note/views.py
+++ 05/Notes/note/views.py
@@ -1,8 +1,22 @@
 from django.views.generic import TemplateView
-from markdown import markdown
 
 from .models import Note
+from pathlib import Path
+from django.views.generic import TemplateView
 
+
+def photo_list():
+    def photo_details(i, f): 
+        if i == 0 : caption = f'Caption for Photo 1 {i}' 
+        elif i == 1: caption = f'Caption for Photo 2 {i}' 
+        elif i == 2: caption = f'Caption for Photo 3 {i}' 
+        elif i == 3: caption = f'Caption for Photo  4{i}' 
+        elif i == 4: caption = f'Caption for Photo {i}' 
+        else: None
+        return dict(id=i, file=f)
+    photos = Path('static/images').iterdir()
+    photos = [photo_details(i, f) for i, f in enumerate(photos)]
+    return photos
 
 class NoteListView(TemplateView):
     template_name = 'notes.html'
@@ -20,3 +34,11 @@ class NoteDetailView(TemplateView):
         return {
             'note': Note.objects.get(pk=kwargs['pk'])
         }
+
+class NoteDetailView(TemplateView):
+    template_name = 'photo.html'
+    def get_context_data(self, **kwargs):
+        photos = photo_list()
+        p = photos[kwargs['pk']]
+        return dict(photo=p)
+    
