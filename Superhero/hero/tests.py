d5fb479eb4dd6d8f8019514ac5f9250e85fe7acb
cohub77 <cohub77@gmail.com>
2022-11-06 09:20:43 -0700

    Added tests cases

---
+++ Superhero/hero/tests.py
@@ -0,0 +1,31 @@
+from django.test import SimpleTestCase, TestCase
+from hero.models import Superhero
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy, reverse
+
+print(Superhero.objects.all())
+heroes = Superhero.objects.all()
+print(len(heroes))
+
+class SuperheroViewsTest(TestCase):
+    
+    def test_hero_add_view(self):
+        a = dict(title='T 1', body='None')
+        b = dict(title='T 2', body='None')
+        response = self.client.post(reverse("hero_add"), a)
+        response = self.client.post(reverse("hero_add"), b)
+users = User.objects.all()
+print(len(users))
+
+class HeroAppTest(SimpleTestCase):
+    def test_django(self):
+        self.assertTrue(True)
+
+
+class UserTest(TestCase):
+    def getUser(self, userList):
+        for user in userList:
+            print(type(user.username))
+
+    def test_user_list(self):
+        self.getUser(users)
