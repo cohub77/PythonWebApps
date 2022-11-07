3f34aba519ea5803a1f00a4fd700ffca5502e602
cohub77 <cohub77@gmail.com>
2022-11-07 07:18:38 -0700

    added .csv to save data

--- Superhero/hero/management/commands/save_data.py
+++ Superhero/hero/management/commands/save_data.py
@@ -1,7 +1,7 @@
 from json import loads, dump
 from django.core.management.base import BaseCommand
 from hero.models import Superhero, Article
-
+from csv import writer
 
 class Command(BaseCommand):
 
@@ -14,8 +14,14 @@ def save_data():
 
     with open('hero_objects.json', "w") as f:
         dump(data, f, indent=4)
-        data = [b for b in Article.objects.all().values()]
+    
+    with open('hero_objects.csv', newline='') as f:
+        writer(f).writerows(data)
+        
+    data = [b for b in Article.objects.all().values()]
 
     with open('article_objects.json', "w") as f:
         dump(data, f, indent=4)
         
+    with open('article_objects.csv', newline='') as f:
+        writer(f).writerows(data)
